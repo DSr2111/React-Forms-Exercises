e87cd4a0487d85965de78892207ecad130eedd5e
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:46:32 -0400

    fixes

--- todos/src/Todo.js
+++ todos/src/Todo.js
@@ -7,40 +7,39 @@ function Todo({ task = "default todo", id = "1", remove, update }) {
   const toggleEdit = () => {
     setIsEditing((edit) => !edit);
   };
-}
 
-const handleChange = (evt) => {
-  setEditTask(evt.target.value);
-};
+  const handleChange = (evt) => {
+    setEditTask(evt.target.value);
+  };
 
-const handleDelete = () => remove(id);
+  const handleDelete = () => remove(id);
 
-const handleUpdate = (evt) => {
-  evt.preventDefault();
-  update(id, editTask);
-  setIsEditing(false);
-};
+  const handleUpdate = (evt) => {
+    evt.preventDefault();
+    update(id, editTask);
+    setIsEditing(false);
+  };
 
-// default todo view
-let jsx = (
-  <div>
-    <li>{task}</li>
-    <button onClick={toggleEdit}>Edit</button>
-    <button onClick={handleDelete}>X</button>
-  </div>
-);
-
-//todo view when editing
-if (isEditing) {
-  jsx = (
+  // default todo view
+  let jsx = (
     <div>
-      <form onSubmit={handleUpdate}>
-        <input type="text" value={editTask} onChange={handleChange} />
-        <button>Update</button>
-      </form>
+      <li>{task}</li>
+      <button onClick={toggleEdit}>Edit</button>
+      <button onClick={handleDelete}>X</button>
     </div>
   );
-}
 
-return jsx;
+  // todo view when editing
+  if (isEditing) {
+    jsx = (
+      <div>
+        <form onSubmit={handleUpdate}>
+          <input type="text" value={editTask} onChange={handleChange} />
+          <button>Update</button>
+        </form>
+      </div>
+    );
+  }
+
+  return jsx;
 }
