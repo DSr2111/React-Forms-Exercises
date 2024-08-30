6f90be4356a41404e86b9e60cc334e71211e1215
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:59:51 -0400

    todoform finish

---
+++ todos/src/NewTodoForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function NewTodoForm({ createTodo }) {
+  const [task, setTask] = useState("");
+
+  const handleChange = (evt) => {
+    setTask(evt.target.value);
+  };
+
+  const gatherInput = (evt) => {
+    evt.preventDefault();
+    createTodo({ task, id: uuid() });
+    setTask("");
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="task">Task :</label>
+        <input
+          id="task"
+          name="task"
+          type="text"
+          onChange={handleChange}
+          value={task}
+        />
+        <button>Add todo item</button>
+      </form>
+    </div>
+  );
+}
