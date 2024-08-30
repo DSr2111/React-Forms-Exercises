b02c9290c1c7eef4fa60add34f84e315e355c225
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:12:14 -0400

    BoxList wip

---
+++ boxes-app/src/BoxList.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Box from "./Box";
+
+function BoxList() {
+  const [boxes, setBoxes] = useState([]);
+  const add = (boxObj) => {
+    setBoxes((boxes) => [...boxes, boxObj]);
+  };
+
+  const remove = (id) => {
+    setBoxes((boxes) => boxes.filter((box) => box.id != id));
+  };
+
+  const boxComponents = boxes.map((box) => <Box />);
+}
