810a83aa4e679931b6da9d3088efabdc1955c468
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:28:11 -0400

    box

---
+++ boxes-app/src/Box.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Box({
+  id,
+  handleRemove,
+  width = 5,
+  height = 5,
+  backgroundColor = "red",
+}) {
+  const remove = () => handleRemove(id);
+  return (
+    <div>
+      <div
+        style={{ height: `${height}em`, width: `${width}em`, backgroundColor }}
+      />
+      <button onClick={remove}>Remove Box</button>
+    </div>
+  );
+}
+
+export default Box;
