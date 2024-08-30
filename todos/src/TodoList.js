a4eab025fee30961c937f67c8d60a2f2687d8d6f
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:49:33 -0400

    todolist start

--- todos/src/TodoList.js
+++ todos/src/TodoList.js
@@ -1,2 +1,17 @@
 import React, { useState } from "react";
 import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const create = (newTodo) => {
+    setTodos((todos) => [...todos, newTodo]);
+  };
+  const update = (id, updatedTask) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task: updatedTask } : undo
+      )
+    );
+  };
+}
