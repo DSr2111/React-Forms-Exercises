7a8b51f03cbdfe4a6e1cefc01cafef7157b1517a
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-30 06:39:45 -0400

    Todo start

--- todos/src/Todo.js
+++ todos/src/Todo.js
@@ -1,1 +1,10 @@
 import React, { useState } from "react";
+
+function Todo({ task = "default todo", id = "1", remove, update }) {
+  const [editTask, setEditTask] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => {
+    setIsEditing((edit) => !edit);
+  };
+}
